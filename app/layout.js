a093ff5d8fd08e51879f565cc660bc9ec2ce9819
jzpoe <pmguez1287@gmail.com>
2024-09-19 08:34:26 -0500

    inicio de proyecto con nextJS

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -21,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="container mx-auto h-screen flex justify-center"
       >
         {children}
       </body>
